2305a89dfedd0719a6ecc858e1613ac8a3dd0ab9
Hải Vũ <haivu@Hais-MacBook-Pro.local>
2024-10-30 17:12:58 +0700

    fix store

--- client/src/store/authStore.ts
+++ client/src/store/authStore.ts
@@ -1,11 +1,14 @@
 import { create } from 'zustand';
 import axios from 'axios';
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
+
 const API_URL = 
   process.env.NODE_ENV === "development" 
     ? `http://localhost:3001/api/auth` 
     : 'https://twohandstore.onrender.com/api/auth';
-console.log(API_URL)
+
+console.log(API_URL);
+
 interface User {
   _id: string;
   name: string;
@@ -17,6 +20,7 @@ interface User {
   gender: string;
   avatar: string | null;
 }
+
 interface AuthState {
   user: User | null;
   token: string | null;
@@ -34,183 +38,178 @@ interface AuthState {
   resetPassword: (password: string) => Promise<void>;
   updateAccount: (id: string, accountData: FormData) => Promise<void>;
   getUserById: (userId: string) => Promise<void>;
-  resendVerificationCode: (email: string)=> Promise<void>
+  resendVerificationCode: (email: string) => Promise<void>;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
-  id: null,
-  user: null,
-  token: null,
-  isAuthenticated: false,
-  isLoading: false,
-  error: null,
-  message: null,
-  getUserById: async (userId) => {
-    set({ isLoading: true, error: null });
-    try {
+const setupAxiosInterceptors = () => {
+  // Set the token in the headers for every request
+  axios.interceptors.request.use(
+    (config) => {
       const token = Cookies.get("token");
-      const response = await axios.get(`${API_URL}/get-user-by-id/${userId}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      set({ user: response.data.user, isLoading: false });
-    } catch (error) {
-      const errorMessage = axios.isAxiosError(error)
-        ? error.response?.data?.message || "Error fetching user"
-        : "An unknown error occurred";
-      set({ error: errorMessage, isLoading: false });
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
     }
-  },
-  signup: async (email, password, name) => {
-    set({ isLoading: true, error: null });
-    try {
-      const response = await axios.post(`${API_URL}/signup`, { email, password, name });
-      set({ user: response.data.user, token: response.data.token, isAuthenticated: true, isLoading: false });
-    } catch (error) {
-      // Kiểm tra xem lỗi có phải là lỗi Axios không
-      const errorMessage = axios.isAxiosError(error)
-        ?  "Có lỗi xảy ra trong quá trình đăng ký" // Truy cập message từ response
-        : "Có lỗi không xác định xảy ra"; // Nếu không phải lỗi Axios
-      set({ error: errorMessage, isLoading: false });
-      throw error; // Ném lỗi ra ngoài để xử lý nếu cần
-    }
-  },
-  
+  );
+};
 
+export const useAuthStore = create<AuthState>((set) => {
+  // Setup Axios interceptors
+  setupAxiosInterceptors();
+
+  return {
+    user: null,
+    token: null,
+    isAuthenticated: false,
+    isLoading: false,
+    error: null,
+    message: null,
+    getUserById: async (userId) => {
+      set({ isLoading: true, error: null });
+      try {
+        const response = await axios.get(`${API_URL}/get-user-by-id/${userId}`);
+        set({ user: response.data.user, isLoading: false });
+      } catch (error) {
+        const errorMessage = axios.isAxiosError(error)
+          ? error.response?.data?.message || "Error fetching user"
+          : "An unknown error occurred";
+        set({ error: errorMessage, isLoading: false });
+      }
+    },
+    signup: async (email, password, name) => {
+      set({ isLoading: true, error: null });
+      try {
+        const response = await axios.post(`${API_URL}/signup`, { email, password, name });
+        set({ user: response.data.user, token: response.data.token, isAuthenticated: true, isLoading: false });
+      } catch (error) {
+        const errorMessage = axios.isAxiosError(error)
+          ? "Có lỗi xảy ra trong quá trình đăng ký"
+          : "Có lỗi không xác định xảy ra";
+        set({ error: errorMessage, isLoading: false });
+        throw error;
+      }
+    },
     signin: async (email, password) => {
       set({ isLoading: true, error: null });
       try {
-          const expirationDate = new Date();
-          expirationDate.setTime(expirationDate.getTime() + (7 * 24 * 60 * 60 * 1000)); // Cookie hết hạn sau 7 ngày
-          const response = await axios.post(`${API_URL}/signin`, { email, password });
-          const { user, token } = response.data; // Giải nén user và token từ phản hồi
-          set({user, token, isAuthenticated: true, isLoading: false });
-          // Sau khi đăng nhập thành công
-          Cookies.set('token', token, { expires: expirationDate }); // Lưu cookie token vào máy tính
+        const expirationDate = new Date();
+        expirationDate.setTime(expirationDate.getTime() + (7 * 24 * 60 * 60 * 1000)); // Cookie hết hạn sau 7 ngày
+        const response = await axios.post(`${API_URL}/signin`, { email, password });
+        console.log('Response from sign in:', response);
+    
+        const { user, token } = response.data; // Giải nén user và token từ phản hồi
 
+        // Log the extracted token to verify it's correct
+        console.log('Extracted token:', token);
+        set({ user, token, isAuthenticated: true, isLoading: false });
+        Cookies.set('token', token, { expires: expirationDate }); // Lưu cookie token vào máy tính
       } catch (error) {
-          const errorMessage = axios.isAxiosError(error) ? 
-              error.response?.data?.message || "Error logging in" : 
-              "An unknown error occurred";
-          set({ error: errorMessage, isLoading: false });
-          throw error;
+        const errorMessage = axios.isAxiosError(error) ? 
+            error.response?.data?.message || "Error logging in" : 
+            "An unknown error occurred";
+        set({ error: errorMessage, isLoading: false });
+        throw error;
       }
-  },
-
-
-  signout: async () => {
-    set({ isLoading: true, error: null });
-    try {
-      document.cookie = 'token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC; secure; SameSite=Lax'; // Xóa cookie token
-      set({ user: null, token: null, isAuthenticated: false, error: null, isLoading: false });
-    } catch (error) {
-      const errorMessage = axios.isAxiosError(error) ? 
-        error.response?.data?.message || "Error logging out" : 
-        "An unknown error occurred";
-      set({ error: errorMessage, isLoading: false });
-      throw error;
-    }
-  },
-
-  verifyEmail: async (code) => {
-    set({ isLoading: true, error: null });
-    try {
-      const response = await axios.post(`${API_URL}/verify-email`, { code });
-      set({ user: response.data.user, token: response.data.token, isAuthenticated: true, isLoading: false });
-    } catch (error) {
-      const errorMessage = axios.isAxiosError(error) ? 
-        error.response?.data?.message || "Error verifying email" : 
-        "An unknown error occurred";
-      set({ error: errorMessage, isLoading: false });
-      throw error;
-    }
-  },
-
-  checkAuth: async () => {
-    set({ isLoading: true, error: null });
-
-    // Không cần lấy token từ cookie, vì backend sẽ tự động lấy token từ cookie
-    try {
+    },
+    signout: async () => {
+      set({ isLoading: true, error: null });
+      try {
+        Cookies.remove('token'); // Xóa cookie token
+        set({ user: null, token: null, isAuthenticated: false, error: null, isLoading: false });
+      } catch (error) {
+        const errorMessage = axios.isAxiosError(error) ? 
+          error.response?.data?.message || "Error logging out" : 
+          "An unknown error occurred";
+        set({ error: errorMessage, isLoading: false });
+        throw error;
+      }
+    },
+    verifyEmail: async (code) => {
+      set({ isLoading: true, error: null });
+      try {
+        const response = await axios.post(`${API_URL}/verify-email`, { code });
+        set({ user: response.data.user, token: response.data.token, isAuthenticated: true, isLoading: false });
+      } catch (error) {
+        const errorMessage = axios.isAxiosError(error) ? 
+          error.response?.data?.message || "Error verifying email" : 
+          "An unknown error occurred";
+        set({ error: errorMessage, isLoading: false });
+        throw error;
+      }
+    },
+    checkAuth: async () => {
+      set({ isLoading: true, error: null });
+      try {
         const response = await axios.get(`${API_URL}/check-auth`, {
-            withCredentials: true, // Đảm bảo rằng cookie được gửi kèm theo yêu cầu
-            headers: {
-                'Content-Type': 'application/json'
-            }
+          withCredentials: true, // Đảm bảo rằng cookie được gửi kèm theo yêu cầu
         });
         set({ user: response.data.user, isAuthenticated: true, isLoading: false });
-    } catch (error) {
-        console.error('Error during checkAuth:', error); // Log lỗi để kiểm tra
+      } catch (error) {
+        console.error('Error during checkAuth:', error);
         const errorMessage = axios.isAxiosError(error) ? 
             error.response?.data?.message || "Error checking authentication" : 
             "An unknown error occurred";
         set({ error: errorMessage, isLoading: false, isAuthenticated: false });
+      }
+    },
+    forgotPassword: async (email) => {
+      set({ isLoading: true, error: null });
+      try {
+        const response = await axios.post(`${API_URL}/forgot-password`, { email });
+        set({ message: response.data.message, isLoading: false });
+      } catch (error) {
+        const errorMessage = axios.isAxiosError(error) ? 
+          error.response?.data?.message || "Error sending reset password email" : 
+          "An unknown error occurred";
+        set({ error: errorMessage, isLoading: false });
+        throw error;
+      }
+    },
+    resetPassword: async (password) => {
+      set({ isLoading: true, error: null });
+      try {
+        const token = new URLSearchParams(window.location.search).get('token');
+        const response = await axios.post(`${API_URL}/reset-password/${token}`, { password });
+        set({ message: response.data.message, isLoading: false });
+      } catch (error) {
+        const errorMessage = axios.isAxiosError(error) ? 
+          error.response?.data?.message || "Error resetting password" : 
+          "An unknown error occurred";
+        set({ error: errorMessage, isLoading: false });
+        throw error;
+      }
+    },
+    updateAccount: async (userId, accountData) => {
+      set({ isLoading: true, error: null });
+      try {
+        const response = await axios.put(`${API_URL}/update-account/${userId}`, accountData, {
+          withCredentials: true,
+        });
+        set({ message: response.data.message, isLoading: false });
+      } catch (error) {
+        const errorMessage = axios.isAxiosError(error)
+          ? error.response?.data?.message || "Error updating account"
+          : "An unknown error occurred";
+        set({ error: errorMessage, isLoading: false });
+        throw error;
+      }
+    },
+    resendVerificationCode: async (email) => {
+      set({ isLoading: true, error: null });
+      try {
+        const response = await axios.post(`${API_URL}/resend-verify-email`, { email });
+        set({ message: response.data.message, isLoading: false });
+      } catch (error) {
+        const errorMessage = axios.isAxiosError(error)
+          ? error.response?.data?.message || "Lỗi khi gửi mã xác thực"
+          : "Đã có lỗi không xác định xảy ra";
+        set({ error: errorMessage, isLoading: false });
+        throw error;
+      }
     }
-},
-
-
-
-  
-
-  forgotPassword: async (email) => {
-    set({ isLoading: true, error: null });
-    try {
-      const response = await axios.post(`${API_URL}/forgot-password`, { email });
-      set({ message: response.data.message, isLoading: false });
-    } catch (error) {
-      const errorMessage = axios.isAxiosError(error) ? 
-        error.response?.data?.message || "Error sending reset password email" : 
-        "An unknown error occurred";
-      set({ error: errorMessage, isLoading: false });
-      throw error;
-    }
-  },
-
-  resetPassword: async (password) => {
-    set({ isLoading: true, error: null });
-    try {
-      const token = new URLSearchParams(window.location.search).get('token');
-      const response = await axios.post(`${API_URL}/reset-password/${token}`, { password });
-      set({ message: response.data.message, isLoading: false });
-    } catch (error) {
-      const errorMessage = axios.isAxiosError(error) ? 
-        error.response?.data?.message || "Error resetting password" : 
-        "An unknown error occurred";
-      set({ error: errorMessage, isLoading: false });
-      throw error;
-    }
-  },
-  updateAccount: async (userId, accountData) => {
-    set({ isLoading: true, error: null });
-    try {
-      const token = Cookies.get('token');
-      console.log(token)
-      if(!token) throw new Error
-      const response = await axios.put(`${API_URL}/update-account/${userId}`, accountData, {
-        withCredentials: true,
-      }); // Pass the user ID in the URL
-      set({ message: response.data.message, isLoading: false });
-    } catch (error) {
-      const errorMessage = axios.isAxiosError(error)
-        ? error.response?.data?.message || "Error updating account"
-        : "An unknown error occurred";
-      set({ error: errorMessage, isLoading: false });
-      throw error;
-    }
-  },
-  resendVerificationCode: async (email) => {
-    set({ isLoading: true, error: null });
-    try {
-      const response = await axios.post(`${API_URL}/resend-verify-email`, { email });
-      set({ message: response.data.message, isLoading: false });
-    } catch (error) {
-      const errorMessage = axios.isAxiosError(error)
-        ? error.response?.data?.message || "Lỗi khi gửi mã xác thực"
-        : "Đã có lỗi không xác định xảy ra";
-      set({ error: errorMessage, isLoading: false });
-      throw error;
-    }
-  }
-  
-  
-}));
+  };
+});
